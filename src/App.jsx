89224ed03773f93bd43910a269d3816fded3a315
Dave <d.ruocco1987@googlemail.com>
2023-01-01 17:15:40 +0000

    Sets up projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import About from './components/About'
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
     <main className="text-gray-300 bg-blue-900 body-font">
       <About />
+      <Projects />
     </main>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Home Page",
+    subtitle: "Typescript",
+    description:
+      "Home page displaying Bitcoin price, local weather and changable backgrounds, utilising the Unsplash, Coingecko and OpenWeatherMap APIs",
+    link: "https://github.com/druoc/home-page",
+  },
+  {
+    id: 2,
+    title: "NC News",
+    subtitle: "Express, Node, PostgreSQL",
+    description:
+      "Reddit-like news site API built as part of the Northcoders backend curriculum",
+    link: "https://github.com/druoc/nc-backend-project",
+  },
+];
