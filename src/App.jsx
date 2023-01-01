ea9675c910547d0b53271f1aaace9a8340ca99db
Dave <d.ruocco1987@googlemail.com>
2023-01-01 16:29:22 +0000

    Sets up basic about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import About from './components/About'
+
 const App = () => {
   return (
-    <main className="text-gray-300 bg-blue-900">
-      <Navbar />
+    <main className="text-gray-300 bg-blue-900 body-font">
+      <About />
     </main>
   )
 }
