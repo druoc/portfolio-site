a5685a30e5e666709d169e67b3f845d592206b27
Dave <d.ruocco1987@googlemail.com>
2023-01-01 18:47:29 +0000

    Set up nav bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="text-gray-300 bg-blue-900 body-font">
+      <Navbar />
       <About />
       <Projects />
       <Technologies />
